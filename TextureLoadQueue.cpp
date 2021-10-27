bd2d04f1502c06f3a2ed10331187d7b157d3bc45
Ed Harry <e.f.harry@gmail.com>
2021-10-27 14:42:42 +0100

    0.2.3-dev
    Signed-off-by: Ed Harry <e.f.harry@gmail.com>

--- TextureLoadQueue.cpp
+++ TextureLoadQueue.cpp
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2019 Ed Harry, Wellcome Sanger Institute
+Copyright (c) 2021 Ed Harry, Wellcome Sanger Institute
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
