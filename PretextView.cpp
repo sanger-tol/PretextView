13f0802c8b5369cfce13567c5594a88a693fcb66
yumisims <yy5@sanger.ac.uk>
2026-04-23 16:22:57 +0100

    change release version

--- PretextView.cpp
+++ PretextView.cpp
@@ -26,7 +26,7 @@ SOFTWARE.
 */
 
 
-#define PretextView_Version_Label "1.0.7-beta"
+#define PretextView_Version_Label "1.0.7"
 #define PretextView_Version "PretextViewAI Version " PretextView_Version_Label
 #define PretextView_Title "PretextViewAI " PretextView_Version_Label " - Wellcome Sanger Institute"
 
